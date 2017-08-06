0e007250395f9364c3744b768eefb49690789b35
simranchauhan <30750435+simranchauhan@users.noreply.github.com>
2017-08-06 22:31:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 var articleOne ={
-     title : " Article-one| Simran Chauhan",
-     heading : "article-one",
+     title : 'Article-one| Simran Chauhan',
+     heading : 'article-one',
      date : ' aug 6,2017',
      content : `        <p>
                     This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
@@ -22,11 +22,13 @@ var articleOne ={
 };
 
 function createTemplate (data){
-    var title= data.title;
-     var date= data.date;
-      var heading= data.heading;
-       var content= data.content;
-var htmltemplate =`<html>
+    var title = data.title;
+     var date = data.date;
+      var heading = data.heading;
+       var content = data.content;
+       
+var htmltemplate =`
+<html>
     <head>
         <title>
             ${title}
@@ -61,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one' , function (req, res ) {
-   res.send(createTemplate(ArticleOne));
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two' , function (req, res ) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
